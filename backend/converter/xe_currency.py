3fc628caaf89a1c3907984b112558025bd1f2452
Adedamola <adedamola@sendme.ng>
2023-01-17 07:48:46 +0100

    UPD - /currencies endpoint.

--- backend/converter/xe_currency.py
+++ backend/converter/xe_currency.py
@@ -1,8 +1,13 @@
 # Stdlib Imports
+import re
 from typing import List, Dict, Any
 
-# Third Party Imports
-import httpx
+# Thirdparty Imports
+import certifi
+import urllib3
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def list_of_currencies() -> List[Dict[str, Any]]:
@@ -12,16 +17,35 @@ def list_of_currencies() -> List[Dict[str, Any]]:
 
     :return: A list of dictionaries.
     """
+    url = "https://help.xe.com/hc/en-gb/articles/4408840388753-Which-currencies-do-you-offer-"
+    # Start a webdriver instance
+    driver = webdriver.Chrome()
 
-    currencies = [
+    # Navigate to the specified URL
+    driver.get(url)
+
+    # Wait for the page to load
+    driver.implicitly_wait(10)
+
+    # Get the HTML content of the page
+    html = driver.page_source
+
+    # Close the webdriver
+    driver.quit()
+
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Find all 'span' tags with class='wysiwyg-color-black'
+    wysiwyg_color_black = soup.find_all("span", class_="wysiwyg-color-black")
+
+    # Extract the text from each tag and return as a list
+    return [
         {
-            "Japanese Yen": "JPY",
-        },
-        {
-            "Kenyan Shilling": "KES",
-        },
+            tag.get_text()
+            .split("(")[0]: tag.get_text()
+            .split("(")[1]
+            .split(")")[0]
+        }
+        for tag in wysiwyg_color_black
     ]
-    return currencies
-
-
-print(list_of_currencies())
